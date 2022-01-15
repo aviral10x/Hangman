7d64aadbb9c0f5571ccd10382817b8166901f1d9
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-01-16 00:45:13 +0530

    Word

--- hangman.py
+++ hangman.py
@@ -1,2 +1,8 @@
 import random
 word_list = [aviral, vaibhav, ishaan, ali]
+guess = random.choice(word_list)
+for letter in chosen_word:
+    if letter == guess:
+        print("Right")
+    else:
+        print("Wrong")    
