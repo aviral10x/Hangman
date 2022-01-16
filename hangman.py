79bf75c52deae14e155d670691b44defab3102e8
Aviral <21f1000608@student.onlinedegree.iitm.ac.in>
2022-01-16 23:14:11 +0530

    guess loop

--- hangman.py
+++ hangman.py
@@ -1,16 +1,22 @@
 import random
 word_list = ["aviral", "vaibhav", "ishaan", "ali"]
 chosen_word = random.choice(word_list)
-guess = input("Guess a letter: ").lower()
+
 display = []
 word_length = len(chosen_word)
 for _ in range(word_length):
     display += "_"
 print(display)
 
-for position in range(word_length):
-    letter = chosen_word[position]
-    if letter == guess:
-        display[position] = letter
+end_of_game = False
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = letter
+    print(display)
 
-print(display)
+    if "_" not in display:
+        end_of_game = True
+        print("You Win")
